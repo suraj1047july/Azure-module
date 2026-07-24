93262817c3b6e3ae70188b71db974dccccb190b8
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 15:33:33 +0530

    Add files via upload

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_virtual_network" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group      = var.resource_group
+  address_space       = var.address_space
+}
---
+++ Vnet/variable.tf
@@ -0,0 +1,5 @@
+variable "name"                { type = string }
+variable "location"            { type = string }
+variable "resource_group"      { type = string }
+variable "address_space"       { type = list(string) }
+

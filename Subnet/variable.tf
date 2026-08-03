2245745a412efc0fb645e7b6531173abac096e58
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-03 13:14:32 +0530

    Create variable.tf

---
+++ Subnet/variable.tf
@@ -0,0 +1,12 @@
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+variable "resource_group" {
+  description = "Resource Group"
+  type        = string
+}
+variable "vnet" {
+  description = "Name of the virtual network"
+  type        = string
+} 

eac8cfe9977a06645ccb913543374f2c910f8574
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-03 12:35:00 +0530

    Update output.tf

--- RouteTable/output.tf
+++ RouteTable/output.tf
@@ -1,3 +1,14 @@
-output "RouteTable_id" {
-  value = azurerm_route_table.this.id
+output "id" {
+  description = "Route Table ID"
+  value       = azurerm_route_table.this.id
+}
+
+output "name" {
+  description = "Route Table Name"
+  value       = azurerm_route_table.this.name
+}
+
+output "resource_group_name" {
+  description = "Resource Group Name"
+  value       = azurerm_route_table.this.resource_group_name
 }

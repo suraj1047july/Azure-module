e3ef53648b1a23416f9a61969b811276e82af194
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-07 16:27:56 +0530

    Update main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -4,3 +4,9 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = var.vnet
   address_prefixes     = var.address_prefixes
   }
+resource "azurerm_subnet_route_table_association" "this" {
+  count = var.route_table_id != null ? 1 : 0
+
+  subnet_id      = azurerm_subnet.this.id
+  route_table_id = var.route_table_id
+}

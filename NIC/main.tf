bcdd6d96aac64b622a1d61d71990e03efc1b829c
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-13 16:00:37 +0530

    Create main.tf

---
+++ NIC/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_network_interface" "this" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group
+
+  ip_configuration {
+    name                          = var.ip_configuration_name
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+    private_ip_address            = var.private_ip_address
+    public_ip_address_id          = var.public_ip_address_id
+  }
+  }

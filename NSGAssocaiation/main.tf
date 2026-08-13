66fe0192c8a3fc91b5fe5771dead7a3b7f2d7d9f
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-13 14:48:30 +0530

    Create main.tf

---
+++ NSGAssocaiation/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = azurerm_subnet.this.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}

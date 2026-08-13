86fab2506e20cefc80ac90563e1b894aee477c19
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-08-13 14:29:56 +0530

    Update main.tf

--- NSG/main.tf
+++ NSG/main.tf
@@ -14,7 +14,3 @@ resource "azurerm_network_security_group" "this" {
     destination_address_prefix = var.destination_address_prefix
   }
   }
-resource "azurerm_subnet_network_security_group_association" "this" {
-  subnet_id                 = azurerm_subnet.this.id
-  network_security_group_id = azurerm_network_security_group.this.id
-}
